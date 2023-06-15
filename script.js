c64cfe9923c643c9355fef7eb8f4b854b8066d05
afrizaldwi <afrizaldwisaga@gmail.com>
2023-06-15 19:18:11 +0700

    contact form update

--- script.js
+++ script.js
@@ -41,38 +41,3 @@ setInterval(() => {
 	slideIndex -= 1;
 	showSlide(slideIndex);
 }, 5000);
-
-// contact
-const scriptURL =
-	"https://script.google.com/macros/s/AKfycbzkr1IaURMGpfMMTzxsX_DJFwNmeUHUdi75t2TnsXrla5TkedcfC5NCBRioBOQdwalU/exec";
-const form = document.forms["contact-form"];
-
-const btnKirim = document.getElementById("btnKirim");
-const btnLoading = document.getElementById("btnLoading");
-const alert = document.getElementById("alert");
-const alertWarning = document.querySelector(".warning");
-
-form.addEventListener("submit", (e) => {
-	e.preventDefault();
-
-	btnKirim.classList.toggle("d-none");
-	btnLoading.classList.toggle("d-none");
-	fetch(scriptURL, { method: "POST", body: new FormData(form) })
-		.then((response) => {
-			alert.classList.toggle("d-none");
-			btnKirim.classList.toggle("d-none");
-			btnLoading.classList.toggle("d-none");
-
-			form.reset();
-			console.log("Success!", response);
-		})
-		.catch((error) => {
-			alertWarning.classList.toggle("d-none");
-			console.error("Error!", error.message);
-		});
-});
-
-const exit = document.getElementById("exit");
-exit.addEventListener("click", () => {
-	alert.classList.add("d-none");
-});
